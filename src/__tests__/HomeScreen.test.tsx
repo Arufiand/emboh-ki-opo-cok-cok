00932ef7051d717737abbb7baa7d6c75b7477010
Alfian Danny Armanta <alfiandannyarmanta@gmail.com>
2025-05-09 10:35:28 +0700

    feat: Create Tests
    Create Testing case for home and weathers components

--- src/__tests__/HomeScreen.test.tsx
+++ src/__tests__/HomeScreen.test.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import HomeScreen from '../HomeScreen';
+import HomeScreen from '../screens/HomeScreen';
 import {render} from '@testing-library/react-native';
 
 describe('Home Screen', () => {
   test('Should render Home Screen', () => {
     const wrapper = render(<HomeScreen />);
     wrapper.getByTestId('home-screen');
+  });
+
+  describe('Title Sections', () => {
+    beforeEach(() => {
+      jest.useFakeTimers('modern');
+      jest.setTimeout(10000);
+    });
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+    test('Should Contain Real Date', () => {
+      const wrapper = render(<HomeScreen />);
+      wrapper.getByText('May 09, 2025');
+    });
+    test('Should Contain Day', () => {
+      const wrapper = render(<HomeScreen />);
+      wrapper.getByText('Friday');
+    });
   });
 });
---
+++ src/components/__tests__/WeatherCurrent.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import WeatherCurrent from '../WeatherCurrent';
+
+describe('Weather Current', () => {
+  test('Should render Weather Current Component', () => {
+    const wrapper = render(<WeatherCurrent />);
+    wrapper.getByTestId('weather-current');
+  });
+});
