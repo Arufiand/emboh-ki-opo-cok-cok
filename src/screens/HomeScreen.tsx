3ed7d51defbcb317f89f2709d37d226f5b0dcb1b
Alfian Danny Armanta <alfiandannyarmanta@gmail.com>
2025-05-09 10:35:04 +0700

    feat: Create Screens
    Create screen and component for Home and Weathers component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,59 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../constants';
+import moment from 'moment';
+
+function displayDate(date: Date) {
+  const today = date.getDate();
+  const month = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ][date.getMonth()];
+
+  return `${month} ${today < 10 && '0'}${today}, ${date.getFullYear()}`;
+}
 
 function HomeScreen() {
-  return <View testID={'home-screen'} />;
+  return (
+    <LinearGradient
+      colors={[Colors.LIGHT_GREY, Colors.DARKER_GREY]}
+      testID="home-screen"
+      style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.date}>{displayDate(moment().toDate())}</Text>
+        <Text style={styles.day}>{moment().format('dddd')}</Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 40,
+    alignContent: 'space-between',
+    justifyContent: 'space-around',
+  },
+  title: {
+    justifyContent: 'flex-end',
+  },
+  date: {
+    color: Colors.GRAY,
+  },
+  day: {
+    color: Colors.WHITE,
+  },
+});
 
 export default HomeScreen;
