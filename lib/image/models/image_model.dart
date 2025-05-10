91e683c1fd24a737a5280f9e38053ac136bfe121
arufiand <alfiandannyarmanta@gmail.com>
2025-05-10 19:23:24 +0700

    feat: Part 95
    fetching image model

--- lib/image/models/image_model.dart
+++ lib/image/models/image_model.dart
@@ -1,19 +1,19 @@
-class ImageModel{
+class ImageModel {
   int id = 0;
   String url = "";
   String title = "";
 
   ImageModel(this.id, this.title, this.url);
 
-  ImageModel.parseJson(Map <String, dynamic> parsed){
+  ImageModel.parseJson(Map<String, dynamic> parsed) {
     id = parsed['id'];
     url = parsed['url'];
     title = parsed['title'];
   }
 
-//   Alternative
-//   ImageModel.parseJsonAlternative(Map<String, dynamic> parsed)
-//     : id = parsed['id'],
-//     url = parsed['url'],
-//     title = parsed['title']
+  //   Alternative
+  //   ImageModel.parseJsonAlternative(Map<String, dynamic> parsed)
+  //     : id = parsed['id'],
+  //     url = parsed['url'],
+  //     title = parsed['title']
 }
--- lib/image/src/app.dart
+++ lib/image/src/app.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' show get;
+import 'package:learning/image/models/image_model.dart';
 
-class App extends StatefulWidget{
+class App extends StatefulWidget {
+  const App({super.key});
+
   @override
-  createState(){
+  createState() {
     return AppState();
   }
 }
 
-class AppState extends State<App>{
+class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() async {
+    counter += 1;
+    var res = await get(
+      Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'),
+    );
+    var imageModel = ImageModel.parseJson(json.decode(res.body));
+
+    setState(() {
+      counter += 1;
+    });
+  }
+
   @override
-  Widget build(context){
-    return  MaterialApp(
-        home: Scaffold(
-          body: Text('$counter'),
-          appBar: AppBar(
-            title: Text('See Images From Custom Lib'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: (){
-              setState(() {
-                counter += 1;
-              });
-            },),
-        )
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        appBar: AppBar(title: Text('See Images From Custom Lib')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
