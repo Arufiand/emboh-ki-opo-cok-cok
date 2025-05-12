b4f086a1e284201d6c5135509f016e5da80c307d
arufiand <alfiandannyarmanta@gmail.com>
2025-05-12 16:44:42 +0700

    feat(login_form): Part 109
    Login Apps

---
+++ lib/login_form/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log Me in',
+      home: Scaffold(body: Text('Login form')),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'image/src/app.dart';
 
-void main(){
+// import 'image/src/app.dart';
+import 'login_form/src/app.dart';
+
+void main() {
   runApp(App());
 }
