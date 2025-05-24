be1ceecd7a887774bb3a12be80a3e48530a3dbfd
arufiand <alfiandannyarmanta@gmail.com>
2025-05-24 14:30:27 +0700

    refactor(login_bloc): Part 167
    i dont know

--- lib/image/src/app.dart
+++ lib/image/src/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'dart:async';
 import 'package:http/http.dart' show get;
 import 'package:learning/image/models/image_model.dart';
 import 'package:learning/image/widgets/image_lists.dart';
@@ -17,6 +16,7 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
   List<ImageModel> images = [];
+
   void fetchImage() async {
     counter += 1;
     final res = await get(
