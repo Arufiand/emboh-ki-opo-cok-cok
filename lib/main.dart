424fd35337d04b15079d4d4d66c8a9e0d74f211e
arufiand <alfiandannyarmanta@gmail.com>
2025-05-31 20:22:40 +0700

    feat(cat_in_the_box_animation): Part 218
    adding PaintDebug

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'cat_in_the_box/src/app.dart';
+
 // import 'package:learning/login_bloc/src/app.dart';
+import 'package:flutter/rendering.dart';
 
 // import 'image/src/app.dart';
 // import 'login_form/src/app.dart';
 
 void main() {
+  debugPaintSizeEnabled = false;
   runApp(App());
 }
