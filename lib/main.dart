b0df5dfe9bbbc6b50748f3f8b81f502879b7fbd4
arufiand <alfiandannyarmanta@gmail.com>
2025-08-16 16:57:43 +0700

    feat: Initiialize project for relearning basic of flutter

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.green[500],
+              borderRadius: BorderRadius.circular(25),
+            ),
+            padding: EdgeInsets.all(25),
+            child: Text(
+              "Mitch KOko",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
