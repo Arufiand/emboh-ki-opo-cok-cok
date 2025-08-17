c518e579bfa01075da64b6606f751ec3ea500be4
arufiand <alfiandannyarmanta@gmail.com>
2025-08-17 14:45:56 +0700

    feat(Flutter_fundamentals): Bottom bar navigation OK

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,36 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:mitch_koko_learning/pages/home_page.dart';
+import 'package:mitch_koko_learning/pages/profile_page.dart';
+import 'package:mitch_koko_learning/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _selectedIndex = 0;
+
+  final List _pages = [HomePage(), SettingsPage(), ProfilePage()];
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("First Page")),
-      drawer: Drawer(
-        backgroundColor: Colors.deepPurple[100],
-        child: Column(
-          children: [
-            DrawerHeader(child: Icon(Icons.favorite, size: 48)),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("H O M E"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/homePage');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("S E T T I N G S"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/settingsPage');
-              },
-            ),
-          ],
-        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
       ),
+      // drawer: Drawer(
+      //   backgroundColor: Colors.deepPurple[100],
+      //   child: Column(
+      //     children: [
+      //       DrawerHeader(child: Icon(Icons.favorite, size: 48)),
+      //       ListTile(
+      //         leading: Icon(Icons.home),
+      //         title: Text("H O M E"),
+      //         onTap: () {
+      //           Navigator.pop(context);
+      //           Navigator.pushNamed(context, '/homePage');
+      //         },
+      //       ),
+      //       ListTile(
+      //         leading: Icon(Icons.settings),
+      //         title: Text("S E T T I N G S"),
+      //         onTap: () {
+      //           Navigator.pop(context);
+      //           Navigator.pushNamed(context, '/settingsPage');
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
       // body: Center(
       //   child: ElevatedButton(
       //     onPressed: () {
