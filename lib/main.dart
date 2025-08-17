3d6d8fb6207e0cd5e5247cf0069fedb09d235939
arufiand <alfiandannyarmanta@gmail.com>
2025-08-17 14:56:46 +0700

    feat(Flutter_fundamentals): statefull learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mitch_koko_learning/pages/first_page.dart';
-import 'package:mitch_koko_learning/pages/home_page.dart';
-import 'package:mitch_koko_learning/pages/settings_page.dart';
+import 'package:mitch_koko_learning/new_pages/counter_pages.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,107 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // void userTapped() {
-  //   print("user Tapped");
-  // }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      home: FirstPage(),
-      routes: {
-        '/firstpage': (context) => FirstPage(),
-        '/homePage': (context) => HomePage(),
-        '/settingsPage': (context) => SettingsPage(),
-      },
-
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text("My App BarS"),
-      //     backgroundColor: Colors.deepPurple,
-      //     elevation: 200,
-      //     leading: Icon(Icons.menu),
-      //     actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-      //   ),
-
-      // body: Center(
-      //   child: GestureDetector(
-      //     onTap: userTapped,
-      //     child: Container(
-      //       height: 300,
-      //       width: 300,
-      //       color: Colors.green,
-      //       child: Center(child: Text("Tap me")),
-      //     ),
-      //   ),
-      // ),
-
-      // body: Stack(
-      //   alignment: Alignment.bottomRight,
-      //   children: [
-      //     Container(
-      //       height: 300,
-      //       width: 300,
-      //       decoration: BoxDecoration(color: Colors.green),
-      //     ),
-      //     Container(
-      //       height: 200,
-      //       width: 200,
-      //       decoration: BoxDecoration(color: Colors.green[400]),
-      //     ),
-      //
-      //     Container(
-      //       height: 100,
-      //       width: 100,
-      //       decoration: BoxDecoration(color: Colors.green[200]),
-      //     ),
-      //   ],
-      // ),
-
-      // body: GridView.builder(
-      //   itemCount: 64,
-      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //     crossAxisCount: 4,
-      //   ),
-      //   itemBuilder: (context, index) =>
-      //       Container(color: Colors.deepPurple, margin: EdgeInsets.all(2)),
-      // ),
-
-      // body: ListView(
-      //   children: [
-      //     Container(
-      //       height: 200,
-      //       decoration: BoxDecoration(color: Colors.green),
-      //     ),
-      //     Container(
-      //       height: 200,
-      //       decoration: BoxDecoration(color: Colors.green[400]),
-      //     ),
-      //
-      //     Container(
-      //       height: 200,
-      //       decoration: BoxDecoration(color: Colors.green[200]),
-      //     ),
-      //   ],
-      // ),
-      // body: Center(
-      // child: Container(
-      //   height: 300,
-      //   width: 300,
-      //   decoration: BoxDecoration(
-      //     color: Colors.green[500],
-      //     borderRadius: BorderRadius.circular(25),
-      //   ),
-      //   padding: EdgeInsets.all(25),
-      //   child: Text(
-      //     "Mitch KOko",
-      //     style: TextStyle(color: Colors.white, fontSize: 28),
-      //   ),
-      // ),
-      // ),
-      // ),
-    );
+    return MaterialApp(home: CounterPages());
   }
 }
---
+++ lib/new_pages/counter_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CounterPages extends StatefulWidget {
+  const CounterPages({super.key});
+
+  @override
+  State<CounterPages> createState() => _CounterPagesState();
+}
+
+class _CounterPagesState extends State<CounterPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
