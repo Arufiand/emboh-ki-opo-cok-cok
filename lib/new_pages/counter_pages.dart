c072b4d20d56fb940d277d3b5feac528b7182af7
arufiand <alfiandannyarmanta@gmail.com>
2025-08-17 15:04:44 +0700

    feat(Statefull_phase): statefull learning

--- lib/new_pages/counter_pages.dart
+++ lib/new_pages/counter_pages.dart
@@ -8,8 +8,30 @@ class CounterPages extends StatefulWidget {
 }
 
 class _CounterPagesState extends State<CounterPages> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("You pushed the button this many times : "),
+            Text(_counter.toString(), style: TextStyle(fontSize: 40)),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text("Increment button"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
