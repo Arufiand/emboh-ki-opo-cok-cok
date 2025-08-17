4f8568894bdc961f86bc013fd983b321c7ff98ac
arufiand <alfiandannyarmanta@gmail.com>
2025-08-17 14:03:13 +0700

    feat(Flutter_fundamentals): Fundamental OK

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,98 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void userTapped() {
+    print("user Tapped");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[300],
+        appBar: AppBar(
+          title: Text("My App BarS"),
+          backgroundColor: Colors.deepPurple,
+          elevation: 200,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
+
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.green[500],
-              borderRadius: BorderRadius.circular(25),
-            ),
-            padding: EdgeInsets.all(25),
-            child: Text(
-              "Mitch KOko",
-              style: TextStyle(color: Colors.white, fontSize: 28),
+          child: GestureDetector(
+            onTap: userTapped,
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.green,
+              child: Center(child: Text("Tap me")),
             ),
           ),
         ),
+
+        // body: Stack(
+        //   alignment: Alignment.bottomRight,
+        //   children: [
+        //     Container(
+        //       height: 300,
+        //       width: 300,
+        //       decoration: BoxDecoration(color: Colors.green),
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       width: 200,
+        //       decoration: BoxDecoration(color: Colors.green[400]),
+        //     ),
+        //
+        //     Container(
+        //       height: 100,
+        //       width: 100,
+        //       decoration: BoxDecoration(color: Colors.green[200]),
+        //     ),
+        //   ],
+        // ),
+
+        // body: GridView.builder(
+        //   itemCount: 64,
+        //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //     crossAxisCount: 4,
+        //   ),
+        //   itemBuilder: (context, index) =>
+        //       Container(color: Colors.deepPurple, margin: EdgeInsets.all(2)),
+        // ),
+
+        // body: ListView(
+        //   children: [
+        //     Container(
+        //       height: 200,
+        //       decoration: BoxDecoration(color: Colors.green),
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       decoration: BoxDecoration(color: Colors.green[400]),
+        //     ),
+        //
+        //     Container(
+        //       height: 200,
+        //       decoration: BoxDecoration(color: Colors.green[200]),
+        //     ),
+        //   ],
+        // ),
+        // body: Center(
+        // child: Container(
+        //   height: 300,
+        //   width: 300,
+        //   decoration: BoxDecoration(
+        //     color: Colors.green[500],
+        //     borderRadius: BorderRadius.circular(25),
+        //   ),
+        //   padding: EdgeInsets.all(25),
+        //   child: Text(
+        //     "Mitch KOko",
+        //     style: TextStyle(color: Colors.white, fontSize: 28),
+        //   ),
+        // ),
+        // ),
       ),
     );
   }
