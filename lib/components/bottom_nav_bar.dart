ee74e8f9574975002c83d4a301fae7631290c14e
arufiand <alfiandannyarmanta@gmail.com>
2025-08-26 22:26:46 +0700

    feat(HomeScreen) : update creating home and cart nav bar

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GNav(
+      color: Colors.grey[400],
+      activeColor: Colors.grey.shade700,
+      tabActiveBorder: Border.all(color: Colors.white),
+      tabBackgroundColor: Colors.grey.shade100,
+      tabs: [
+        GButton(icon: Icons.home, text: "Shop"),
+        GButton(icon: Icons.shopping_bag_rounded, text: "Cart"),
+      ],
+    );
   }
 }
