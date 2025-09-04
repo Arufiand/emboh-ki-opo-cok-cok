849255a5ac93bb9459315987a6a2e566229dfc1b
arufiand <alfiandannyarmanta@gmail.com>
2025-09-04 22:56:10 +0700

    feat(ShopScreen) : updating screen for shop screen

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -5,6 +5,52 @@ class ShopScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Shop Screen"));
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text("Search", style: TextStyle(color: Colors.grey)),
+                Icon(Icons.search, color: Colors.grey),
+              ],
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text("Everyone Flies with Garuda Indonesia"),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "Hot Picks 🔥",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                "See All",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
