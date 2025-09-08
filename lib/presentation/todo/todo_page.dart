a74174da51fca3ba45deb6c08a7112e8c9030daa
arufiand <alfiandannyarmanta@gmail.com>
2025-09-08 16:45:59 +0700

    feat(todoPresentation): Todo page and view baseline

---
+++ lib/presentation/todo/todo_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mitch_koko_bloc_learning/domain/repository/todo_repo.dart';
+import 'package:mitch_koko_bloc_learning/presentation/todo/todo_cubit.dart';
+import 'package:mitch_koko_bloc_learning/presentation/todo/todo_view.dart';
+
+class TodoPage extends StatelessWidget {
+  final TodoRepo todoRepo;
+  const TodoPage({super.key, required this.todoRepo});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TodoCubit(todoRepo),
+      child: TodoView(),
+    );
+  }
+}
