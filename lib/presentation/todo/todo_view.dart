29b1de1d37161ffc91c4216494b7961c3160a709
arufiand <alfiandannyarmanta@gmail.com>
2025-09-08 21:59:49 +0700

    feat(todoPresentation): belum jadi

--- lib/presentation/todo/todo_view.dart
+++ lib/presentation/todo/todo_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mitch_koko_bloc_learning/presentation/todo/todo_cubit.dart';
 
 class TodoView extends StatelessWidget {
   const TodoView({super.key});
 
+  void _showAddTodoBox(BuildContext context) {
+    final todoCubit = context.read<TodoCubit>();
+    final textController = TextEditingController();
+    
+    showDialog(context: context, builder: builder)
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _showAddTodoBox(context),
+      ),
+    );
   }
 }
