9e619bef5a7bf992349971383ff4239b8f5ca433
arufiand <alfiandannyarmanta@gmail.com>
2025-09-09 07:21:56 +0700

    feat(todoPresentation): adding view page
    update todoview for showing dialog

--- lib/presentation/todo/todo_view.dart
+++ lib/presentation/todo/todo_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mitch_koko_bloc_learning/presentation/todo/todo_cubit.dart';
 
 class TodoView extends StatelessWidget {
@@ -7,8 +8,26 @@ class TodoView extends StatelessWidget {
   void _showAddTodoBox(BuildContext context) {
     final todoCubit = context.read<TodoCubit>();
     final textController = TextEditingController();
-    
-    showDialog(context: context, builder: builder)
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(controller: textController),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              todoCubit.addTodo(textController.text);
+              Navigator.of(context).pop();
+            },
+            child: const Text("Add"),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
